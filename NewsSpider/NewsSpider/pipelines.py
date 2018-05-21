21e4e509a5993e5d80d434f7f7be26cc3b370805
zzygit <zzygit@gmail.com>
2018-05-22 00:46:15 +0800

    v3

--- NewsSpider/NewsSpider/pipelines.py
+++ NewsSpider/NewsSpider/pipelines.py
@@ -12,7 +12,7 @@ import settings
 class NewsspiderPipeline(object):
     def __init__(self):
         # 连接复制集
-        self.client = pymongo.MongoClient('107.172.188.221:27017', replicaset='my_repl')
+        self.client = pymongo.MongoClient('107.175.184.250:27017', replicaset='my_repl')
         # 获得数据库
         self.db = self.client['News']  
         # 获得集合
