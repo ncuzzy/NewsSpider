d10153a20fd3ae78888603ee0d37de4b21f1c615
ncuzzy <zzygit@gmail.com>
2018-05-22 18:51:52 +0000

    Update middlewares.py

--- NewsSpider/NewsSpider/middlewares.py
+++ NewsSpider/NewsSpider/middlewares.py
@@ -22,7 +22,7 @@ class MyproxiesSpiderMiddleware(object):
 
       def __init__(self):  
         self.proxy=[]
-        r = requests.get('http://107.172.188.221:8000/?types=0&count=10&country=国内')
+        r = requests.get('http://107.175.184.250:8000/?types=0&count=10&country=国内')
         ip_ports = json.loads(r.text)
         for i in range (0,10):
             s = "http://"+ip_ports[i][0]+r":"+str(ip_ports[i][1])
